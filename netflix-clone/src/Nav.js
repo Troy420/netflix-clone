650ecaa032b45bd962adc8d49678606cb954ba82
Bernard <bernardy_ho@hotmail.com>
2022-01-06 20:54:03 +0700

    created nav

--- netflix-clone/src/Nav.js
+++ netflix-clone/src/Nav.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Nav.css";
 
 const Nav = () => {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
   return (
     <>
-      <div className="nav">
+      <div className={`nav ${show && "nav__black"}`}>
         <img
           className="nav__logo"
           src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
